a646be00a428337478425b5efcf7bb6cae80a274
aswan97 <123411360+aswan97@users.noreply.github.com>
2023-01-24 09:41:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,26 @@ app = Flask(_name_)
 
 @app.route("/")
 def index():
-  player = request.args.get("player", "")
-  if player:
-    result = r_p_s(player)
+  play = request.args.get("play", "")
+  if play == "Yes":
+    player = request.args.get("player", "")
+    while play == "Yes":
+      r_p_s(player)
+      play = request.args.get("play", "")
+    else:
+      print("We hope you change your mind!")
   else:
     result = ""
   return (
     """<form action="" method="get">
-              <input type="text" name="player">
+              <input type="text" name="play">
+              <input type="submit" value="Convert">
+              </form>"""
+      + play
+  )
+  return (
+    """<form action="" method="get">
+              <input type="text" name="play">
               <input type="submit" value="Convert">
               </form>"""
       + result
@@ -21,42 +33,47 @@ def index():
     
 @app.route("/<str:player>")
 def r_p_s(player):
-  """create a list of play options"""
-  t = ["Rock", "Paper", "Scissors"]
-
-  """assign a random play to the computer"""
-  computer = t[randint(0, 2)]
-
-  """set player to False"""
-  player = False
-
-  while player == False:
-    """set player to True"""
-    player = input("Rock", "Paper", "Scissors?")
+    """create a list of play options"""
+    t = ["Rock", "Paper", "Scissors"]
+    """assign a random play to the computer"""
+    computer = t[randint(0, 2)]
+    """player = input("Rock", "Paper", "Scissors?")"""
     if player == computer:
       result = print("Tie!")
+      return result
     elif player == "Rock":
       if computer == "Paper":
-        result = print ("The machines win this round", player, "gets covered by", computer)
+        result = print ("The machines win this round", player, "gets covered by", computer,".")
+        return result
       else:
-        result = print ("You win!", player, "decimates", computer)
+        result = print ("You win!", player, "decimates", computer,".")
+        return result
     elif player == "Paper":
       if computer == "Scissors":
-        result = print ("You lose, shame on you.", player, "is shredded by", computer)
+        result = print ("You lose, shame on you.", player, "is shredded by", computer,".")
+        return result
       else:
-        result = print ("Congrats! You win.", player, "covers", computer)
+        result =  print ("Congrats! You win.", player, "covers", computer,".")
+        return result
     elif player == "Scissors":
       if computer == "Rock":
-        result = print ("You lose, shame on you.", player, "gets beaten down by", computer)
+        result = print ("You lose, shame on you.", player, "gets beaten down by", computer,".")
+        return result
       else:
-        result = print ("Congrats! You win.", player, "shreds", computer)
+        result = print ("Congrats! You win.", player, "shreds", computer,".")
+        return result
     else:
-  result = print ("That is not a valid choice, please check your spelling!")
-  
-  return str(result)
-   
-  """player = False
-  computer = t[randint(0,2)]"""
+        result = print ("That is not a valid choice, please check your spelling!")
+        return result
+
+#play = input("Would you like to play Rock, Paper, Scissors?: ")
+#while play == "Yes":
+    #player = input("Please enter Rock, Paper, or Scissors: ")
+    #r_p_s(player)
+    #play = input("Would you like to play again? ")
+#else:
+    print("We hope you change your mind!")
+
 
 if _name_ == "_main_":
   app.run(host = "127.0.0.1", port = 8080, debug = True)
